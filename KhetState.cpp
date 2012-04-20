2c08a1db435d252526c3a22ca1086279b5d1ebe5
Aysylu <aysylu7@gmail.com>
2012-04-20 19:54:40 -0400

    Merged two if-statements for checking legality of the move

--- KhetState.cpp
+++ KhetState.cpp
@@ -471,8 +471,7 @@ long KhetState::gen()
               if (piece.color == RED) {
                 if (toFile == 9) continue;
                 if (toFile == 1 && (toRank == 0 || toRank == 7)) continue;
-              }
-              if (piece.color == SILVER) {
+              } else {
                 if (toFile == 0) continue;
                 if (toFile == 8 && (toRank == 0 || toRank == 7)) continue;
               }
